87366915644106b3bddfabe93e1711641e124658
Cracklitosss <marroquinca17@gmail.com>
2024-04-18 04:30:15 -0600

    Preparacion aws

--- index.ts
+++ index.ts
@@ -2,11 +2,20 @@ import express from 'express';
 import signale from 'signale';
 import userReceivedRoutes from './src/user/infrastructure/UserReceivedRoutes';
 import * as dotenv from 'dotenv';
-
+import cors from 'cors'; 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+ 
+// Configuración de CORS detallada
+app.use(cors({
+    origin: '*', // Ajusta esto adecuadamente en un entorno de producción
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true // Ten cuidado con esta opción si el origen es '*'
+}));
+
 app.use(express.json());
 app.use('/user-received', userReceivedRoutes);
 
